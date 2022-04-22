8df12d01b73d7011d543def5aa21aa1ad6ef5e05
Enes <66560757+devenes@users.noreply.github.com>
2022-04-22 21:19:43 +0300

    Update files

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -85,8 +85,8 @@ WSGI_APPLICATION = 'cblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': '',  # database name in RDS is written here
-        'USER': '',  # database master username in RDS is written here
+        'NAME': 'database1',  # database name in RDS is written here
+        'USER': 'admin',  # database master username in RDS is written here
         'PASSWORD': config('PASSWORD'),
         # database endpoint is written here
         'HOST': 'aws-capstone-rds.cukd79ofsohr.us-east-1.rds.amazonaws.com',
--- userdata.sh
+++ userdata.sh
@@ -3,7 +3,7 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN="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"
+TOKEN="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"
 git clone https://$TOKEN@github.com/devenes/my-aws-capstone-project.git
 cd /home/ubuntu/my-aws-capstone-project
 apt install python3-pip -y
